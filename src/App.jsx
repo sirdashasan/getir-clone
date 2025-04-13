ca622b6d1034e03dd3168f2e74f323e09c9305fa
sirdashasan <sirdashasan@hotmail.com>
2025-04-13 12:57:52 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-['Cocon-Regular'] text-4xl font-bold text-green-500">
+      Getir Clone
+    </div>
+  );
+}
+
+export default App;
